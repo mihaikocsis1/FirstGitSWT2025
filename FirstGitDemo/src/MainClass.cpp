f37834be595e2bbca544f5885e4afdd7f4571f6a
Mihai Kocsis <mihai.kocsis@hs-heilbronn.de>
2025-12-19 10:06:42 +0100

    Initial commit

---
+++ FirstGitDemo/src/MainClass.cpp
@@ -0,0 +1,15 @@
+/*
+ * MainClass.cpp
+ *
+ *  Created on: 19.12.2025
+ *      Author: mihai
+ */
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	cout << "Git Test" << endl;
+	return 0;
+}
+
